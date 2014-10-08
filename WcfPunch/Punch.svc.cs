cae4d93ced114f8e9575a7a1e7a60bde76dfc9f8
Serge Lapointe <serge_lapointe@hotmail.com>
2014-10-08 13:50:29 -0400

    Nouveau changement

--- WcfPunch/Punch.svc.cs
+++ WcfPunch/Punch.svc.cs
@@ -14,7 +14,7 @@ namespace WcfPunch
     {
         public string GetData(int value)
         {
-            return string.Format("You entered: {0}", value);
+            return string.Format("Entre une valeur: {0}", value);
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
